a0f0dd29779272d6c9dd49c6420b69feac090810
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-23 11:33:08 +0300

    deleted: delete all for planing

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import Header from './Header'
-import ListItem from './ListItem'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Header />
-        <ListItem />
-      </div>
+      <div></div>
     </div>
   )
 }
