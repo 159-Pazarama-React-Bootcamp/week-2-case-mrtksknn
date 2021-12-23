48ab813660bb4a57be7772eafcbe06d88cc60ef3
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-23 16:23:19 +0300

    added: default functions for items

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const TodoItem = ({ id, content, isCompleted }) => {
+  const handleCheckboxClick = () => {}
+
+  const handleDeleteClick = () => {}
+
   return (
     <li
       className={`list-group-item ${isCompleted && 'list-group-item-success'}`}
@@ -8,10 +12,17 @@ const TodoItem = ({ id, content, isCompleted }) => {
     >
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center">
-          <input type="checkbox" className="mr-3" checked={isCompleted} />
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={isCompleted}
+            onClick={handleCheckboxClick}
+          />
           {content}
         </span>
-        <button className="btn btn-danger">Delete</button>
+        <button onClick={handleDeleteClick} className="btn btn-danger">
+          Delete
+        </button>
       </div>
     </li>
   )
