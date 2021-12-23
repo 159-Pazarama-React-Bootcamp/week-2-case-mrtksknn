51fa0de4fb0ef8c27a254b3e386c6fc80daf93b9
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-23 12:25:54 +0300

    added: axios request with async await

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [todosData, setTodosData] = useState({})
+
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get('https://61c42343f1af4a0017d99378.mockapi.io/todos')
+        .then((res) => {
+          const { data } = res
+          const newTdoosData = {}
+          data.forEach((todos, index) => {
+            newTdoosData[index + 1] = {
+              id: index + 1,
+              isCompleted: todos.isCompleted,
+              content: todos.content,
+            }
+          })
+          setTodosData(newTdoosData)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+    getData()
+  }, [])
+
+  const getTodos = (todoId) => {
+    const { content } = todosData[todoId]
+
+    return (
+      <div key={todoId}>
+        <li>{content}</li>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <>
+      <div>
+        {todosData ? (
+          <div>{Object.keys(todosData).map((todoId) => getTodos(todoId))}</div>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
+    </>
   )
 }
 
