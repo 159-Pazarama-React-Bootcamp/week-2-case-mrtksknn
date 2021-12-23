364d1430f766aa14a05626cfba55533d8d4664ae
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-23 16:14:53 +0300

    created: completed items counter

--- todo-app/src/components/TodoCompleteItems.js
+++ todo-app/src/components/TodoCompleteItems.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TodoCompleteItems = () => {
-  return <div>vrds</div>
+  return <h4 className="mt-3">Total Complete Items: 5</h4>
 }
 
 export default TodoCompleteItems
