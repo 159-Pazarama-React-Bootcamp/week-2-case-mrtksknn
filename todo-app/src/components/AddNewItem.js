b58d352a15128f195de8b92a266e6bd709dcf5e0
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-24 10:58:04 +0300

    added: redux and store files

--- todo-app/src/components/AddNewItem.js
+++ todo-app/src/components/AddNewItem.js
@@ -9,15 +9,14 @@ const AddNewItem = () => {
   }
 
   return (
-    <form onSubmit={onSubmit} className="form-inline mt-3 mb-3">
+    <form onSubmit={onSubmit} className="form-inline mt-3 mb-3 d-flex">
       <input
         type="text"
         className="form-control mb-2 mr-sm-2"
         placeholder="Add todo..."
         value={value}
         onChange={(event) => setValue(event.target.value)}
-      ></input>
-
+      />
       <button type="submit" className="btn btn-primary mb-2">
         Submit
       </button>
