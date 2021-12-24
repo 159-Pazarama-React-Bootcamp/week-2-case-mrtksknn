5f5b1b22e7269ed76bc26f02aba9f3b792535889
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-24 12:05:33 +0300

    created: fetching data function and display

--- todo-app/src/redux/todoSlice.js
+++ todo-app/src/redux/todoSlice.js
@@ -1,11 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+export const getTodosAsync = createAsyncThunk(
+  'todos/getTodoasync',
+  async () => {
+    const response = await fetch(
+      'https://61c42343f1af4a0017d99378.mockapi.io/todos'
+    )
+    if (response.ok) {
+      const todos = await response.json()
+      return { todos }
+    }
+  }
+)
 
 const todoSlice = createSlice({
   name: 'todos',
-  initialState: [
-    { id: 1, isCompleted: false, content: 'todo 1' },
-    { id: 2, isCompleted: true, content: 'todo 2' },
-  ],
+  initialState: [],
 
   reducers: {
     addTodo: (state, action) => {
@@ -22,6 +32,11 @@ const todoSlice = createSlice({
     },
     deleteTodo: (state, action) => {
       return state.filter((todo) => todo.id !== action.payload.id)
+    },
+  },
+  extraReducers: {
+    [getTodosAsync.fulfilled]: (state, action) => {
+      return action.payload.todos
     },
   },
 })
