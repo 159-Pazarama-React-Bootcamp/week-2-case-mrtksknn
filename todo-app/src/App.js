b596fd115fc05563a3ad2088015644a4f7eb9ce4
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-24 10:52:51 +0300

    created: start version of project

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import AddTodoForm from './components/AddNewItem'
+import TodoList from './components/TodoList'
 
 const App = () => {
   return (
     <div className="container bg-white p-4 mt-5">
-      <h1>My Todo List</h1>
+      <h1>Mert's Todo List</h1>
+      <AddTodoForm />
+      <TodoList />
     </div>
   )
 }
